b3c786b9a7e10712cc0a1f8ebd03df9b7f138dbe
Madison May <madison.may@students.olin.edu>
2013-12-02 22:34:43 -0500

    Get request accessible

--- app.js
+++ app.js
@@ -59,6 +59,34 @@ app.post('/find', function(req, res) {
   });
 });
 
+app.get('/:number', function(req, res) {
+    // Download the Node helper library from twilio.com/docs/node/install
+  // These vars are your accountSid and authToken from twilio.com/user/account
+  var accountSid = 'AC5d2d4a989ff8286db2955049233379b9';
+  var authToken = "9621f4fd26aa65dc95306a3c9deb5785";
+  var client = require('twilio')(accountSid, authToken);
+  var coords = [];
+   
+  client.messages.list(function(err, data) {
+
+      data.messages.forEach(function(message) {
+          if (message.from === req.params.number) {
+            try {
+              var lat_str = message.body.split(",")[0];
+              var lng_str = message.body.split(",")[1];
+              var lat = parseFloat(lat_str);
+              var lng = parseFloat(lng_str);
+              coords.push({'lat': lat, 'lng': lng});
+            } catch (e) {
+              console.log(e);
+            }
+          }
+      });
+      console.log(coords);
+      res.json(coords);
+  });
+})
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
